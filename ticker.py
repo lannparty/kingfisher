deb63cf3079f9f66c5f53199b6818d4ab518f3b1
infinite-turtles <infinite-turtles@users.noreply.github.com>
2021-03-24 18:10:29 -0600

    intial poc

---
+++ ticker.py
@@ -0,0 +1,43 @@
+import pathlib
+import json
+import sys
+
+from yahooquery import Ticker
+
+
+def get_symbols() -> list[dict[str, str]]:
+    stocks_json = pathlib.Path(__file__).parent / "data" / "stocks.json"
+    symbols = []
+    with open(stocks_json, "r") as f:
+        stocks = json.load(f)
+        for stock in stocks["data"]["rows"]:
+            symbol = stock["symbol"]
+            if symbol.isalpha():
+                symbols.append(symbol)
+    return symbols
+
+
+def get_bid_ask(symbols: list):
+    ticker = Ticker(symbols, validate=True, progress=True)
+    ticker_details = ticker.summary_detail
+    spreads = []
+    try:
+        for symbol in symbols:
+            bid = ticker_details[symbol].get("bid")
+            ask = ticker_details[symbol].get("ask")
+            if bid and ask:
+                spread = ask - bid
+                print(f"{symbol} {spread}", file=sys.stderr)
+                return (symbol, bid, ask, spread)
+            else:
+                continue
+    except Exception as e:
+        print(f"{symbol} - Error {e}", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    symbols = get_symbols()
+    spreads = get_bid_ask(symbols)
+    spreads.sort(key=lambda i: i[3])
+    for spread in spreads:
+        print(spread)
