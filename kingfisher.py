34c7568d6a05682e14c8f7d11ad616fc88cb1ace
Luan Truong <luanqtruong@gmail.com>
2021-04-03 12:35:20 -0700

    State machine

--- kingfisher.py
+++ kingfisher.py
@@ -1,53 +1,87 @@
 from ib_insync import *
 from transitions import Machine
 from yahooquery import Ticker
-import logging
 
 class Merchant(object):
     states = ['rest', 'greedy', 'fearful']
 
-    def __init__(self, target, quantity, price_step, fraction_of_spread, wait_for_fill):
+    def __init__(self, target, quantity, price_step, fraction_of_spread, wait_for_fill, bought, sold):
         self.target = target
         self.quantity = quantity
         self.price_step = price_step
         self.fraction_of_spread = fraction_of_spread
         self.wait_for_fill = wait_for_fill
-        
-        self.bought = 0
-        self.sold = 0
-        
-        self.machine = Machine(model=self, states=Merchant.states, initial='rest')
-        self.machine.add_transition('advance', 'content', 'greedy', before='scout', after='buy_low')
-        self.machine.add_transition('advance', 'greedy', 'fearful', before='scout', after='sell_high')
-        self.machine.add_transition('advance', 'fearful', 'content')
+        self.bought = bought
+        self.sold = sold
 
+        self.stock = Contract(symbol=target, exchange='SMART', secType='STK', currency='USD')
+        self.reset()
+
+    def reset(self):
+        self.buy_quantity = self.quantity
+        self.sell_quantity = self.quantity
+    
     def scout(self):
         ticker = Ticker(self.target, validate=True)
         ticker_details = ticker.summary_detail
-        bid = ticker_details[self.target].get("bid")
-        ask = ticker_details[self.target].get("ask")
-        spread = round((ask - bid), 2)
-        return bid, ask, spread
+        self.bid = ticker_details[self.target].get("bid")
+        self.ask = ticker_details[self.target].get("ask")
+        self.spread = round((self.ask - self.bid), 2)
     
     def buy_low(self):
-        bid, ask, spread = self.scout()
-        order = Contract(symbol=self.target, exchange='SMART', secType='STK', currency='USD')
-        max_price = round(bid + (spread * self.fraction_of_spread), 2)
-        while bid < max_price:
-            
-        
+        max_price = round(self.bid + (self.spread * self.fraction_of_spread), 2)
+        print("Bidding up from bid of", self.bid, "to max price of", max_price, "in steps of", self.price_step, "with buy quantity of", self.buy_quantity)
+        trade = ib.placeOrder(self.stock, LimitOrder('BUY', self.buy_quantity, self.bid))
+        ib.sleep(1)
+        order = trade.order
+        while self.bid < max_price:
+            timer = 0
+            while timer < self.wait_for_fill:
+                ib.sleep(1)
+                print("Submitted price:", self.bid, "status:", trade.orderStatus.status)
+                timer += 1
+            self.bid = round(self.bid + self.price_step, 2)
+            order.lmtPrice = self.bid
+            if trade.orderStatus.remaining == 0:
+                self.buy_quantity = trade.orderStatus.remaining
+                return
+            else:
+                trade = ib.placeOrder(self.stock, order)
+        self.buy_quantity = trade.orderStatus.remaining
+    
+    def sell_high(self):
+        min_price = round(self.ask - (self.spread * self.fraction_of_spread), 2)
+        print("Asking down from ask of", self.ask, "to min price of", min_price, "in steps of", self.price_step, "with buy quantity of", self.quantity)
+        trade = ib.placeOrder(self.stock, LimitOrder('SELL', self.quantity, self.ask))
+        ib.sleep(1)
+        order = trade.order
+        while self.ask > min_price:
+            timer = 0
+            while timer < self.wait_for_fill:
+                ib.sleep(1)
+                print("Submitted price:", self.ask, "status:", trade.orderStatus.status)
+                timer += 1
+            self.ask = round(self.ask - self.price_step, 2)
+            order.lmtPrice = self.ask
+            if trade.orderStatus.remaining == 0:
+                self.sell_quantity = trade.orderStatus.remaining
+                return
+            else:
+                trade = ib.placeOrder(self.stock, order)
+        self.sell_quantity = trade.orderStatus.remaining
 
-if __name__ == "__main__":
-    import logging
+ib = IB()
+clientId = 1
+ib.connect('127.0.0.1', 7497, clientId=1)
+USLM = Merchant("USLM", 10, .05, 1, 30, 0, 0)
 
-    logging.basicConfig(level=logging.DEBUG)
-    logging.getLogger("transitions").setLevel(logging.INFO)
+machine = Machine(USLM, ['content', 'greedy', 'fearful'], initial='content')
+machine.add_transition('buy', 'content', 'greedy', before='scout', after='buy_low')
+machine.add_transition('buy', 'greedy', 'greedy', before='scout', after='buy_low')
+machine.add_transition('sell', 'greedy', 'fearful', before='scout', after='sell_high')
+machine.add_transition('sell', 'fearful', 'fearful', before='scout', after='sell_high')
+machine.add_transition('stop', 'fearful', 'content', before='reset')  
 
-    trader = Trader(ticker="GME", quantity=1, price_step=0.1, wait_fill=30)
-    trader.get_graph().draw('my_state_diagram.png', prog='dot')
-    while True:
-        trader.advance()
-        
-USLM = Merchant("USLM", 10, .05, .35, 30)
-USLM.buy_low()
-print(USLM.state)
+USLM.buy()
+USLM.sell()
+USLM.stop()
