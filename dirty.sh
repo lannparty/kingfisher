4286c4df2f4c968493f706dfb700d31685d8a4b6
Luan Truong <luanqtruong@gmail.com>
2021-04-09 07:25:00 -0700

    template

--- dirty.sh
+++ dirty.sh
@@ -11,24 +11,19 @@ EOF
 
 for TARGET in ${TARGETS}
 do
-cat << EOF >> docker-compose.yaml
-  ${TARGET}-buy_first-${DATE}:
-    image: ${IMAGE}
-    container_name: ${TARGET}-buy_first-${DATE}
-    network_mode: host
-    environment:
-    - KINGFISHER_TARGET=${TARGET}
-    - KINGFISHER_CLIENT_ID=\${CLIENT_ID}
-    - KINGFISHER_STRATEGY=buy_first
-  ${TARGET}-sell_first-${DATE}:
-    image: ${IMAGE}
-    container_name: ${TARGET}-sell_first-${DATE}
-    network_mode: host
-    environment:
-    - KINGFISHER_TARGET=${TARGET}
-    - KINGFISHER_CLIENT_ID=\${CLIENT_ID}
-    - KINGFISHER_STRATEGY=sell_first
+  for i in `seq 1 2`
+  do
+    cat << EOF >> docker-compose.yaml
+  ${TARGET}-\${STRATEGY}-${DATE}:
+  image: ${IMAGE}
+  container_name: ${TARGET}-${STRATEGY}-${DATE}
+  network_mode: host
+  environment:
+  - KINGFISHER_TARGET=${TARGET}
+  - KINGFISHER_CLIENT_ID=\${CLIENT_ID}
+  - KINGFISHER_STRATEGY=\${STRATEGY}
 EOF
+  done
 done
 
 counter=1
@@ -38,6 +33,23 @@ do
   sed -i 0,/\${CLIENT_ID}/s//$counter/ docker-compose.yaml
   counter=$((counter+1))
   cat docker-compose.yaml |grep '${CLIENT_ID}' > /dev/null
+done
+
+cat docker-compose.yaml |grep '${STRATEGY}' > /dev/null
+until [ $? -eq 1 ] 
+do
+  for i in `seq 1 2`
+  do
+    if [ $i -eq 1 ]
+    then
+      sed -i 0,/\${STRATEGY}/s//buy_first/ docker-compose.yaml
+      sed -i 0,/\${STRATEGY}/s//buy_first/ docker-compose.yaml
+    else
+      sed -i 0,/\${STRATEGY}/s//sell_first/ docker-compose.yaml
+      sed -i 0,/\${STRATEGY}/s//sell_first/ docker-compose.yaml
+    fi
+  done
+  cat docker-compose.yaml |grep '${STRATEGY}' > /dev/null
 done
 
 docker-compose up -d
